84e2a85afec4602f28dd39a3c5d8e313b535f99a
Sanjeev Gwachha <gwachhas123@gmail.com>
2022-08-18 17:02:07 +0545

    main-container updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Footer from "./component/footer/Footer";
 import Navbar from "./component/navbar/Navbar";
+import "./App.css"
 
 function App() {
   return (
-   <>
+   <div className="app-container">
    <Navbar />
    <Footer />
-   </>
+   </div>
   );
 }
 
