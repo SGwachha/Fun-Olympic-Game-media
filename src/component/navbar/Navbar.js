0a9964226c925bb809aa129695318fddf2e8e7be
Sanjeev Gwachha <gwachhas123@gmail.com>
2022-08-22 10:34:04 +0545

    updated

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -12,6 +12,7 @@ import {
   Stack,
   useColorMode,
   Center,
+  Text
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
@@ -21,7 +22,9 @@ export default function Navbar() {
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Box>Media</Box>
+          <Box>
+            <Text>Fun Olympics Games</Text>
+          </Box>
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={7}>
               <Button onClick={toggleColorMode}>
