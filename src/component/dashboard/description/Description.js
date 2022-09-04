b37c736018db4fbadc3059eac2ae6a8e7f566564
sanjeev gwachha <gwachhas123@gmail.com>
2022-09-04 15:27:26 +0545

    updated description page

--- src/component/dashboard/description/Description.js
+++ src/component/dashboard/description/Description.js
@@ -1,5 +1,5 @@
-import { Flex, Button, Box, Stack } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { Flex, Button, Box, Stack, Text, Heading } from "@chakra-ui/react";
+import React from "react";
 import SearchBar from "../../navbar/SearchBar";
 import { useNavigate } from "react-router-dom";
 
@@ -13,22 +13,54 @@ const Description = () => {
       </Flex>
       <Stack
         w="90%"
-        gap={20}
+        gap={10}
+        h="100%"
         justify="center"
         align={"center"}
         display={"flex"}
         flexDirection="row"
       >
         <Box
+          w="80%"
+          pos={"relative"}>
+        <Box
           h={"650px"}
-          w="70%"
+          w="100%"
           bg={"gray.100"}
           pos={"relative"}
           cursor="pointer"
         >
           video
         </Box>
-        <Box cursor={"pointer"} h={"650px"} w="30%" bg={"gray.100"}>
+        <Flex mt="3%" justify={"space-between"} w="100%" bg="black">
+          <Box bg="red" w="50%">
+          <Heading fontSize={"24px"}>
+            Football
+          </Heading>
+          <Text>
+            Messi vs Ronaldo
+          </Text>
+          </Box>
+          <Box display={"flex"} gap="10%" w="50%">
+          <Text fontSize={"18px"} cursor="pointer">
+            Like
+            </Text>
+            <Text fontSize={"18px"} cursor="pointer">
+            Dislike
+            </Text>
+            <Text fontSize={"18px"} cursor="pointer">
+            Comment
+            </Text>
+            <Text fontSize={"18px"} cursor="pointer">
+            Share
+            </Text>
+            <Text fontSize={"18px"} cursor="pointer">
+            Report
+            </Text>
+          </Box>
+        </Flex>
+        </Box>
+        <Box cursor={"pointer"} h={"750px"} w="30%" bg={"gray.100"}>
           Suggested
         </Box>
       </Stack>
